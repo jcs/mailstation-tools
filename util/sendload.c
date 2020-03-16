b4076fa4a70dfcba0bc84d5800aad4f8734f920b
joshua stein <jcs@jcs.org>
2020-03-16 16:08:09 -0500

    sendload: support sending through serial port

--- util/sendload.c
+++ util/sendload.c
@@ -6,24 +6,67 @@
  * assumes Loader has been loaded on the Mailstation and is running
  */
 
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-#include <err.h>
-#include <errno.h>
+#include <termios.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #include "tribble.h"
+
+static int serial_fd = -1;
+static int serial_speed = B115200;
 
 void
 usage(void)
 {
-	printf("usage: %s [-dr] [-p port address] <file to send>\n",
-	    getprogname());
-	exit(1);
+	errx(1, "usage: %s [-dr] [-p lpt address | serial device] "
+	    "[-s serial speed] <file to send>", getprogname());
+}
+
+int
+sendbyte_shim(char b)
+{
+	if (serial_fd >= 0)
+		return (write(serial_fd, &b, 1) != 1);
+	else
+		return sendbyte(b);
+}
+
+void
+setupserial(speed_t speed)
+{
+	struct termios tty;
+
+	if (tcgetattr(serial_fd, &tty) < 0)
+		err(1, "tcgetattr");
+
+	cfsetospeed(&tty, speed);
+	cfsetispeed(&tty, speed);
+
+	tty.c_cflag |= (CLOCAL | CREAD);	/* ignore modem controls */
+	tty.c_cflag &= ~CSIZE;
+	tty.c_cflag |= CS8;			/* 8-bit characters */
+	tty.c_cflag &= ~PARENB;			/* no parity bit */
+	tty.c_cflag &= ~CSTOPB;			/* only need 1 stop bit */
+
+	/* setup for non-canonical mode */
+	tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	tty.c_oflag &= ~OPOST;
+
+	/* fetch bytes as they become available */
+	tty.c_cc[VMIN] = 1;
+	tty.c_cc[VTIME] = 1;
+
+	if (tcsetattr(serial_fd, TCSANOW, &tty) != 0)
+		err(1, "tcsetattr");
 }
 
 int
@@ -33,20 +76,35 @@ main(int argc, char *argv[])
 	struct stat sb;
 	unsigned int sent = 0, size = 0, raw = 0;
 	int ch;
-	char *fn;
+	char *fn, *serial_dev = NULL;
 
-	while ((ch = getopt(argc, argv, "dp:r")) != -1) {
+	while ((ch = getopt(argc, argv, "dp:rs:")) != -1) {
 		switch (ch) {
 		case 'd':
 			tribble_debug = 1;
 			break;
 		case 'p':
-			tribble_port = (unsigned)strtol(optarg, NULL, 0);
-			if (errno)
-				err(1, "invalid port value");
+			if (optarg[0] == '/') {
+				if ((serial_dev = strdup(optarg)) == NULL)
+					err(1, "strdup");
+				serial_fd = open(serial_dev,
+				    O_RDWR | O_NOCTTY | O_SYNC);
+				if (serial_fd < 0)
+					err(1, "can't open %s", optarg);
+			} else {
+				tribble_port = (unsigned)strtol(optarg, NULL,
+				    0);
+				if (errno)
+					err(1, "invalid port value");
+			}
 			break;
 		case 'r':
 			raw = 1;
+			break;
+		case 's':
+			serial_speed = (unsigned)strtol(optarg, NULL, 0);
+			if (errno)
+				err(1, "invalid serial port speed value");
 			break;
 		default:
 			usage();
@@ -58,7 +116,10 @@ main(int argc, char *argv[])
 	if (argc != 1)
 		usage();
 
-	checkio();
+	if (serial_fd >= 0)
+		setupserial(serial_speed);
+	else
+		checkio();
 
 	fn = argv[0];
 	pFile = fopen(fn, "rb");
@@ -71,19 +132,24 @@ main(int argc, char *argv[])
 	/* we're never going to send huge files */
 	size = (unsigned int)sb.st_size;
 
-	printf("[port 0x%x] sending %s (%d bytes)...", tribble_port, fn, size);
+	if (serial_fd >= 0)
+		printf("[%s]", serial_dev);
+	else
+		printf("[lpt port 0x%x]", tribble_port);
+
+	printf(" sending %s (%d bytes)...", fn, size);
 	fflush(stdout);
 
 	/* loader expects two bytes, the low and then high of the file size */
 	if (!raw) {
-		if (sendbyte(size & 0xff) != 0)
+		if (sendbyte_shim(size & 0xff) != 0)
 			errx(1, "sendbyte failed");
-		if (sendbyte((size >> 8) & 0xff) != 0)
+		if (sendbyte_shim((size >> 8) & 0xff) != 0)
 			errx(1, "sendbyte failed");
 	}
 
 	while (sent < size) {
-		if (sendbyte(fgetc(pFile)) != 0)
+		if (sendbyte_shim(fgetc(pFile)) != 0)
 			errx(1, "sendbyte failed at %d/%d", sent, size);
 
 		if (sent++ == 0)
